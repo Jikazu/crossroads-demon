6af7181502c3503cbb271e6aac4b647834de0df4
Gabriella Köhler <gabriella.kohler91@gmail.com>
2023-07-22 15:28:59 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Hello, and welcome to the Crossroads where I, Crowley, is the crossroads demon. Are you here to make a deal?")
+x = input("yes/no:")
+
