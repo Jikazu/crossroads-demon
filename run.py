02e0b7da9c2f15d9cc01244fc38957cdcad23ba1
Gabriella Köhler <gabriella.kohler91@gmail.com>
2023-07-22 15:49:58 +0000

    added game start function and calling it so it can run

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-print("Hello, and welcome to the Crossroads where I, Crowley, is the crossroads demon. Are you here to make a deal?")
-x = input("yes/no:")
 
+def game_start():
+    """
+    The function to start the game, with intro text.
+    """
+    #while game_intro = True:
+    print("Hello, and welcome to the Crossroads where I, Crowley, is the crossroads demon. Are you here to make a deal?")
+    deal_choice = input(">" )
+
+    if deal_choice == "yes":
+        print("Ok, please tell me what you want.")
+    elif deal_choice == "no":
+        print("Thank you for playing. Goodbye")
+        restart()
+    else:
+        print("Invalid choice. Please select yes or no.")
+
+game_start()
