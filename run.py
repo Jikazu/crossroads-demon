18075e519d91ccfc9c1a23e83057231c0d9750fe
Gabriella Köhler <gabriella.kohler91@gmail.com>
2023-07-24 20:32:51 +0000

    updated names of the functions to me more conventional

--- run.py
+++ run.py
@@ -2,9 +2,7 @@ def game_start():
     """
     The function to start the game, with intro text.
     """
-    choices = ["yes", "no"]
-    deal_choice = ""
-    while deal_choice not in choices:
+    while True:
         print("You have stumbled upon a crossroads, where the legend says if you dig a hole in the dead center of the crossroads, and bury a box containing a picture")
         print("of yourself, graveyard dirt and a bone from a black cat, a crossroads demon can be summoned.")
         print("When making a deal with a demon, they can make your dreams come true.")
@@ -13,8 +11,9 @@ def game_start():
         print('"Hello mortal, it is I, the crossroads demon Crowley. Tell me, who have summoned me here?"')
         name = input("> ")
         print("Hello, " + name+ ". Ok, Are you here to strike a deal with me?" )
-        choiceOne(name)
+        choice_one(name)
         print("Do you want to try again?")
+        print("Options: Yes/No")
         while True: 
             again = input("> ")
             if again.lower() == "no":
@@ -25,45 +24,55 @@ def game_start():
                 print("Invalid choice. Please select yes or no")
         
 
-def choiceOne(name):
-
+def choice_one(name):
+    """
+    The function that brings you to select if you want to make a deal or not.
+    """
     while True:
         print("Options: yes/no")
         deal_choice = input("> ")
         if deal_choice.lower() == "yes" :
             print("Please tell me what you want. Most of you humans either want one of two things. Love, or money.")
-            choiceLovemoney()
+            choice_lovemoney()
             break
-        elif deal_choice == "no":
+        elif deal_choice.lower() == "no":
             print("Thank you for playing. Goodbye")
             break
         else:
             print("Invalid choice. Please select yes or no.")
 
-def choiceLovemoney():
+def choice_lovemoney():
+    """
+    The function that brings you to the first dealchoice for love or money.
+    """
     while True:
+        print("Options: love/money")
         deal_choice = input("> ")
-        if deal_choice == "love":
+        if deal_choice.lower() == "love":
             print("Oh, you're after love I see. Your wish is granted!")
             print("You leave the crossroads and go back home. The day after, you're going out with your friend to a pub.")
             print("In the corner of the pub, you see a group of people, that you and your friend starts to talk to.")
             print("During the rest of the evening, two people out of the group have been flirting with you heavily during the night.")
             print("Their names are Harold and Anna, and both of them has asked you out for a date. Which one do you want to go out on a date with?")
-            loveChoice()
+            love_choice()
             break
-        elif deal_choice == "money":
+        elif deal_choice.lower() == "money":
             print("You're a greedy bastard arent you.")
             break
         else: 
             print("Invalid choice. Please select love or money")
 
-def loveChoice():
+def love_choice():
+    """
+    The function that is called if you made the choice love
+    """
     while True:
+        print("Options: Harold/Anna")
         deal_choice = input("> ")
-        if deal_choice == "Harold":
+        if deal_choice.lower() == "harold":
             print("You decided to go out with Harold. The date was a success, and he turned out to be your soulmate.")
             break
-        elif deal_choice == "Anna":
+        elif deal_choice.lower() == "anna":
             print("You decided to go out with Anna. The date was a success, and she turned out to be your soulmate.")
             break
         else:
